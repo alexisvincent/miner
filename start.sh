410c03b834b549f7a8fa960401eb7676fd61a21b
Alexis Vincent <mail@alexisvincent.io>
2017-11-28 03:31:13 +0200

    Switched away from nicehash

---
+++ start.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/fireice-uk/xmr-stak.git
+mkdir xmr-stak/build
+cd xmr-stak/build
+cmake .. -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF
+make install
+
+cp /etc/config.txt /xmr-stak/build/config.txt
+
+./bin/xmr-stak  --currency monero -u 45ZacSit94ViVbE4suy5z544WNrzeKdnU3hRzxzw2Yab6e5kfRojEUMWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm43Ffqss -p worker:mail@alexisvincent.io --url pool.supportxmr.com:3333
---
+++ test.sh
@@ -0,0 +1,1 @@
+docker run -it -e USERNAME=1DodPqJqf8bHB6kVoQ4ifomDfJgSJ7ib8p -e PASSWORD=x -e URL=stratum+tcp://cryptonight.eu.nicehash.com:3355 condevtec/xmr-miner-cpu
